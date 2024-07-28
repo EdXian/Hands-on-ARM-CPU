ce401fb1701292f580dbaf6ecfc48888eb0441c1
pinxian <f29993856@gmail.com>
2024-07-28 21:45:46 +0800

    update day2-section in cortex-M

--- cortex-m/day2_section/start2.s
+++ cortex-m/day2_section/start2.s
@@ -3,8 +3,8 @@
 
 .global vector_start
 vector_start:
-stack_top: .word 0x20002000
- //.word stack_top
+//stack_top: .word 0x20002000
+ .word stack_top
 reset_entry: .word Reset_Handler + 1   //why Reset Handler + 1
 reserved: .space 0x400
 
