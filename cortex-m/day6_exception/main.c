040df81f34a9d6b3e6117388a16e5314771a35d0
pinxian <f29993856@gmail.com>
2024-08-05 20:58:32 +0800

    update day6

--- cortex-m/day6_exception/main.c
+++ cortex-m/day6_exception/main.c
@@ -1,19 +1,9 @@
 
 
 void UsageFault_Handler(void){
-/*
-  __asm volatile(
-    "LDR r2, =0xE000E00C    \n" // Load Coprocessor Power Control Register
-    "MOV R3, #0x00000000    \n"
-    "STR R3, [R2]           \n"
-    "DSB                    \n"
-    "ISB                    \n"
-    "ADD R0, R0, #1         \n" // Increase flag if UsageFault has happened
-    "BX  LR                 \n"
-  );
-*/
 
   *((int*)0xE000ED88) |= 0x00F00000 ;
+
 }
 
 
@@ -29,6 +19,7 @@ int main(void){
   // a = *(int*)0xFFFFFFFF ;
 
   /*Usage fault*/ 
+ *((int*)0xE000ED88) &= ~(0x00F00000) ;
   *(int*)0xE000ED24 |= 2 << 17; 
  float c=1.0,b=1.0;
   b=b/c;
